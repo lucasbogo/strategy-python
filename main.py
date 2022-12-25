efcc2968ead6b57f41ed241b3ebe520eab6380fb
lucasbogo <lucasbogo93@gmail.com>
2022-12-25 19:46:07 -0300

    it works

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ from abc import ABC, abstractmethod
 
 class CalculateShippingStrategy(ABC):
     @abstractmethod
-    def calculate_shipping(self, price, weight):
+    def calculate_shipping(self, price, weight, total):
         pass
 
 
 class CalculateShippingByAir(CalculateShippingStrategy):
-    def calculate_shipping(self, price, weight):
+    def calculate_shipping(self, price, weight, total):
         price = 30
-        return price * weight * 2
+        total = price + weight * 2
+        return total
 
 class CalculateShippingByLand(CalculateShippingStrategy):
-    def calculate_shipping(self, price, weight):
+    def calculate_shipping(self, price, weight, total):
         price = 20
-        return price * weight * 0.5
+        total = price + weight * 0.5
+        return total
+
+class Shipping:
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    def calculate_shipping(self, price, weight, total):
+        return self._strategy.calculate_shipping(price, weight, total)
+
+if __name__ == "__main__":
+    shipping = Shipping(CalculateShippingByAir())
+    print(shipping.calculate_shipping(10, 2, 0))
+
+    shipping = Shipping(CalculateShippingByLand())
+    print(shipping.calculate_shipping(10, 2, 0))
+
